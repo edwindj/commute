ba9b0334e05a76cfcc6f5ecc4b8b747915403440
Edwin de Jonge <edwindjonge@gmail.com>
2017-06-15 22:09:50 +0200

    adding line.R

---
+++ line.R
@@ -0,0 +1,28 @@
+library(sf)
+
+centers <- st_read("data/centers.geojson")
+banen <- read.csv("data/banen.csv")
+
+banen$woon_i <- match(banen$woon, centers$code)
+banen$werk_i <- match(banen$werk, centers$code)
+coord <- st_coordinates(centers)
+
+# interpoleer tussen de woon en werk afstand (s tussen 0 en 1)
+create_lines <- function(banen, coord, s=0){
+  woon <- banen$woon_i
+  werk <- banen$werk_i
+  value <- banen$banen
+  sfc <- lapply(1:nrow(banen),function(i){
+    st_linestring(coord[c(woon[i], werk[i]),])
+  }) %>% st_sfc
+  st_sf(banen=value, sfc) 
+}
+
+a <- create_lines(banen, coord, s=0.1)
+
+library(tmap)
+
+plot(a, col=adjustcolor("red", alpha.f = 0.01))
+tm_shape(a) + tm_lines()
+
+st_make_grid()
