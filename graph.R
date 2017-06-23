98ba209c041ed4a040a73e64f2e925cd5fe2a078
Edwin de Jonge <edwindjonge@gmail.com>
2017-06-23 09:20:29 +0200

    wip

--- graph.R
+++ graph.R
@@ -1,8 +1,49 @@
 dat <- read.csv("data/banen.csv")
 
+library(dplyr)
+centers <- sf::st_read("data/centers.geojson", stringsAsFactors = FALSE) %>% 
+  rename(label = name)
 library(igraph)
-g <- igraph::graph_from_data_frame(dat)
-g
-plot(g)
 
-V(g)$name
+g_w <- igraph::graph_from_data_frame(dat)
+
+
+commuters <- 
+  dat %>% 
+  filter(werk != woon)
+
+g_commuters <- igraph::graph_from_data_frame(commuters)
+
+idx <- match(V(g_commuters)$name, centers$code)
+
+V(g_commuters)$label <- centers$label[idx]
+
+l <- cbind(x=centers$X, y=centers$Y)[idx,]
+
+#plot(g_commuters)
+
+E(g_commuters)$weight <- E(g_commuters)$banen
+
+
+c_commuters <- cluster_label_prop(g_commuters)
+
+com <- communities(c_commuters)
+res <- lapply(names(com), function(n){
+  code <- com[[n]]
+  group <- rep(n, length(code))
+  data.frame(group = group, code=code, stringsAsFactors = F)
+}) %>% bind_rows()
+
+V(g_commuters)$cluster <- as.integer(res$group[match(V(g_commuters)$name, res$code)])
+
+
+pal <- RColorBrewer::brewer.pal(12, "Set3")
+pal <- c(pal, pal)
+V(g_commuters)$color <- pal[as.integer(V(g_commuters)$cluster)]
+V(g_commuters)$color
+
+V(g_commuters)$X <- 1
+igraph.options(verbose=TRUE)
+igraph.options(vertex.label.cex=0.4)
+
+plot(g_commuters, layout=l)
--- interpolate.R
+++ interpolate.R
@@ -3,30 +3,77 @@ library(sf)
 centers <- st_read("data/centers.geojson")
 banen <- read.csv("data/banen.csv")
 
+library(dplyr)
+
+woon_tot <- 
+  banen %>% 
+  group_by(code = woon) %>% 
+  summarise(inwoners = sum(banen))
+
+werk_tot <- 
+  banen %>% 
+  group_by(code = werk) %>% 
+  summarise(werknemers = sum(banen))
+
+woon_commuters <- 
+  banen %>% 
+  filter(woon != werk) %>% 
+  group_by(code = woon) %>% 
+  summarise(commuters_woon = sum(banen))
+
+werk_commuters <- 
+  banen %>% 
+  filter(woon != werk) %>% 
+  group_by(code = werk) %>% 
+  summarise(commuters_werk = sum(banen))
+
+centers <- 
+  centers %>% 
+  left_join(woon_tot) %>% 
+  left_join(werk_tot) %>% 
+  left_join(woon_commuters) %>% 
+  left_join(werk_commuters)
+
+
 banen$woon_i <- match(banen$woon, centers$code)
 banen$werk_i <- match(banen$werk, centers$code)
 coord <- st_coordinates(centers)
 
 # interpoleer tussen de woon en werk afstand (s tussen 0 en 1)
-interpolate <- function(banen, coord, s=0){
+interpolate <- function(banen, coord, centers, s=0){
   woon <- banen$woon_i
   werk <- banen$werk_i
   value <- banen$banen
   pos <- (1-s)*coord[woon, ] + s * coord[werk,]
+  # animeer ook de bubble in iedere plaats: corrigeert voor het feit dat bubbles over elkaar geplot worden.
+  # note, dit werkt niet voor meerkleurige plots....
+  in_place <- (woon == werk)
+  value[in_place] = 0
+  #value[in_place] = ((1-s)*(centers$commuters_woon[woon]) + s*(centers$commuters_werk[werk]))[in_place]
   st_sf(banen=value, st_cast(st_sfc(st_multipoint(pos)), "POINT")) 
 }
 
-a <- interpolate(banen,coord, s=0.1)
 
+a <- interpolate(banen,coord, centers, s=0.1)
+#View(a)
 library(tmap)
 
-png("img/p%03d.png")
+tm_shape(a) + 
+   tm_bubbles("banen", col="red", border.lwd=0) + tm_legend(legend.show=FALSE)
 
-for (i in seq(0, 1, by=0.05)){
-  a <- interpolate(banen, coord, s=i)
+
+png("img/p%03d.png")
+snapshot <- 
+  seq(0, 1, by=0.05) %>% 
+  tail(-1) %>% 
+  head(-1) %>% 
+  c(., rev(.))
+
+for (i in snapshot){
+  a <- interpolate(banen, coord, centers, s=i)
   p <- 
     tm_shape(a) + 
-    tm_bubbles("banen", col="red")
+    tm_bubbles("banen", col="red", border.lwd=0, alpha=0.6) + tm_legend(legend.show=FALSE)
   print(p)
 }
 
@@ -34,6 +81,7 @@ dev.off()
 
 library(magick)
 im <- image_read(list.files("img", "*.png", full.names = T))
-im <- c(im, tail(rev(im),-1))
 an <- image_animate(image_join(im))
-image_write(an, "test.gif")
+image_write(an, "nl.gif")
+
+
